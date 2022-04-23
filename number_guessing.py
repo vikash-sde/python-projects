db0359fbcdf8676e34119583708f4cc3f78d9bde
Vikash Gupta <vikas.gupta@ext.zestmoney.in>
2022-04-23 14:36:14 +0530

    project update

---
+++ number_guessing.py
@@ -0,0 +1,37 @@
+import random
+
+upper_limit = input('type upper limit number: ')
+
+if upper_limit.isdigit() :
+    upper_limit = int(upper_limit)
+    if upper_limit <=0 :
+        print('please type valid number greater than 0')
+        quit()
+else:
+    print('please type a number next time')
+    quit()
+    
+random_number = random.randrange(0,upper_limit)
+print(random_number)
+
+guesses = 0
+
+while True :
+    user_guess = input('make a guess: ')
+    guesses+=1
+
+    if user_guess.isdigit() :
+        user_guess = int(user_guess)
+      
+    else:
+        print('please type a number next time')
+        continue
+    if user_guess == random_number:
+        print('you got it right!')
+        break
+    else:
+        print('you got it wrong!')
+    
+
+
+print('you got it in',guesses,'guesses')
